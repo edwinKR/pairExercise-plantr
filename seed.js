a5387fe848454c6b468262c9fc613a46ecfc634a
Edwin Kim <edwin.k.kim@gmail.com>
2018-11-12 17:21:21 -0600

    In Process: Promise chaining

--- seed.js
+++ seed.js
@@ -1,18 +1,44 @@
-const db = require('./model');
+const { db, Vegetable, Plot, Gardener, PlotVegetable } = require('./model');
 
-db.sync({force: true})
-    .then(() => {
-        console.log('Database synced!')
-    })
-    .catch(err => {
-        console.log('Disaster! Something went wrong! ');
-        console.log(err);
-    
-    })
-    .finally(() => {
-        db.close();
-    })
+const vegetableData = [
+  {
+    name: 'Carrot',
+    color: 'Orange'
+  },
+  {
+    name: 'Lettuce',
+    color: 'Green'
+  }
+];
 
+const gardenerData = [
+  { name: 'McGregor', age: 60 },
+  { name: 'Hashimoto', age: 37 },
+  { name: 'Giancarlo', age: 19 }
+];
 
+const plotData = [
+  {
+    size: 10,
+    shaded: true
+  },
+  {
+    size: 40,
+    shaded: false
+  }
+];
 
-// db.close()
+db.sync({ force: true })
+  .then(() => {
+    console.log('Database synced!');
+    return Promise.all([
+      Vegetable.bulkCreate(vegetableData),
+      Gardener.bulkCreate(gardenerData),
+      Plot.bulkCreate(plotData)
+    ]);
+  })
+  .catch(err => {
+    console.log('Disaster! Something went wrong! ');
+    console.log(err);
+    db.close();
+  });
